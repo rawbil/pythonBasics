11673b95ac1dfc414f7ffc6ecd18b39e2d2d573c
Bil <bildadsimiyu6@gmail.com>
2025-01-27 12:38:35 +0300

    add a check for negative numbers

--- factorial.py
+++ factorial.py
@@ -1,5 +1,8 @@
 n = int(input("Enter value of n: "))
-f = 1
-for i in range(1, n + 1):
-    f = f*i
-print("Factorial result is: ", f)
+if n < 0:
+    print(f"{n} should be greater than 0")
+else:
+    f = 1
+    for i in range(1, n + 1):
+        f = f*i
+    print(f"Factorial of {n} is: ", f)
