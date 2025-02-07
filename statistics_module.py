c9ed542599f3a00095861c84b6f010f14fc41d44
Bil <bildadsimiyu6@gmail.com>
2025-02-07 23:07:00 +0300

    generating otp and using statistics module to perform mean, median and mode
    Signed-off-by: Bil <bildadsimiyu6@gmail.com>

---
+++ statistics_module.py
@@ -0,0 +1,7 @@
+# find mean, mode and median for a given set of numbers in a list
+from statistics import mean, median, mode
+
+list1 = [10, 95, 92, 19, 50, 37]
+print(f"Mean is: {mean(list1)}")
+print(f"median is: {median(list1)}")
+print(f"Mode is: {mode(list1)}")
