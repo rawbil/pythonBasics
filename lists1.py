6d40f2d87603f3ba098dc032f351a49fbeadf562
Bil <bildadsimiyu6@gmail.com>
2025-03-16 13:57:12 +0300

    refactor: add list methods
    Signed-off-by: Bil <bildadsimiyu6@gmail.com>

--- lists1.py
+++ lists1.py
@@ -7,3 +7,20 @@ print(choose)
 
 myList = [1, 34, 85, 58]
 print(myList[:])  # creates a copy of the list from beginning to end
+print(myList[::2])  # alternates the elements at intervals of 2
+
+L1 = [1, 2, 3]
+L2 = [20, 30, 40]
+L3 = L1 + L2
+print(L3)
+L4 = L1 * 10
+print(L4)
+L1.extend(L2)
+print(L1)
+
+# sort
+L1.sort()
+print(L1)
+# reverse sort
+L1.sort(reverse=True)
+print(L1)
