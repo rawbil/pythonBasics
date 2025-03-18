928685feddcea0b507eaf9d6a430bc8c8751e3ee
Bil <bildadsimiyu6@gmail.com>
2025-03-18 15:50:14 +0300

    string functions
    Signed-off-by: Bil <bildadsimiyu6@gmail.com>

---
+++ strings_basics.py
@@ -0,0 +1,22 @@
+"""message = "Hello"
+for i in message:
+    print(i)
+
+index = 0
+for i in range(index, len(message)):
+    print(message[i])
+
+"""
+
+# isalnum
+str1 = "hello"
+print(str1.isalnum())
+print(str1.isalpha())
+print(str1.isdigit())
+print(str1.islower())
+print(str1.isupper())
+print(str1.isspace())
+print(str1.capitalize())
+print(str1.strip())
+print(str1.lstrip())  # leading whitespace
+print(str1.rstrip())  # trailing whitespace
